05e2d0eb4a087323111e560a78ac0e9c68fcaa3d
Rifal777 <rifalfato7@gmail.com>
2025-01-02 20:09:43 +0700

    tambahan pertemuan 11
    lanjutan

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.room_persistence"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.room_persistence"
-        minSdk = 24
-        targetSdk = 34
+        minSdk = 31
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -52,4 +52,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("androidx.room:room-ktx:$room_version")
+
+    implementation ("com.google.android.material:material:1.8.0")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+
+    implementation ("androidx.lifecycle:lifecycle-viewmodel:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
 }
--- app/src/main/java/com/example/room_persistence/MainActivity.kt
+++ app/src/main/java/com/example/room_persistence/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.room_persistence
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.InvalidationTracker
+import com.example.room_persistence.room.MainViewModel
+import com.example.room_persistence.room.NoteAdapter
+import com.example.room_persistence.ui.AddNoteActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+   private  lateinit var noteAdapter: NoteAdapter
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +28,24 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        recyclerView=findViewById(R.id.recyclerViewNotes)
+        recyclerView.layoutManager= LinearLayoutManager(this)
+
+        mainViewModel= ViewModelProvider(this).get(mainViewModel::class.java)
+
+        mainViewModel.allNotes.observe(this, Observer {notes ->
+            noteAdapter= NoteAdapter(notes)
+            recyclerView.adapter=noteAdapter
+        })
+
+
+
+        val buttonAddNote: Button =findViewById(R.id.buttonAddNote)
+        buttonAddNote.setOnClickListener{
+            val intent= Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
         }
     }
 }
